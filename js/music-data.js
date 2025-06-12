7ec32635842913a68007c626c842e964e6440d79
dlmkjadragon <gogobao21022006@gmail.com>
2025-06-13 01:54:04 +0700

    Update music-data.js

--- js/music-data.js
+++ js/music-data.js
@@ -4,238 +4,238 @@ const musicList = [
     title: "Megalo Strike Back",
     artist: "Sapph",
     cover: "img/music/megalo.png",
-    src: "music/Megalo Strike Back - Sapph.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752893/Megalo_Strike_Back_-_Sapph_hfoawp.mp3"
   },
   {
     id: "silence",
     title: "Silence",
     artist: "Kai Engel",
     cover: "img/music/kai_engel.png",
-    src: "music/Silence - Kai Engel.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752896/Silence_-_Kai_Engel_g247fp.mp3"
   },
   {
     id: "curtains",
     title: "Curtains Are Always Drawn",
     artist: "Kai Engel",
     cover: "img/music/kai_engel.png",
-    src: "music/Curtains Are Always Drawn - Kai Engel.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752887/Curtains_Are_Always_Drawn_-_Kai_Engel_wshx00.mp3"
   },
   {
     id: "ijwd",
     title: "I Just Wanna Die",
     artist: "Keedfour",
     cover: "img/music/ijwd.png",
-    src: "music/ijwd- Keedfour.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752890/ijwd-_Keedfour_hzjj8z.mp3"
   },
   {
     id: "iroh",
     title: "Iroh",
     artist: "Sonn",
     cover: "img/music/iroh.jpg",
-    src: "music/Iroh - Sonn.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752889/Iroh_-_Sonn_yfqpue.mp3"
   },
   {
     id: "lafterdeath",
     title: "Love After Death",
     artist: "Music Box",
     cover: "img/music/lad.png",
-    src: "music/lafterdeath.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752889/lafterdeath_lsryue.mp3"
   },
   {
     id: "hitorinbo",
     title: "Hitorinbo Envy",
     artist: "Madotsuki",
     cover: "img/music/hitomadot.png",
-    src: "music/Hitorinbo Envy - Madotsuki.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752897/Hitorinbo_Envy_-_Madotsuki_s64zeq.mp3"
   },
   {
     id: "hitorinb",
     title: "Hitorinbo Envy",
     artist: "Koyori",
     cover: "img/music/hitokoyori.png",
-    src: "music/Hitorinb Envy - Koyori.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752890/Hitorinb_Envy_-_Koyori_gtlahl.mp3"
   },
   {
     id: "melancholia",
     title: "Melancholia",
     artist: "Music Box",
     cover: "img/music/melancholia.png",
-    src: "music/Melancholia (Music Box).mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752894/Melancholia_Music_Box_qyvunp.mp3"
   },
   {
     id: "siriuss",
     title: "Sirius no Shinzou",
     artist: "Isekaijouchou",
     cover: "img/music/isekaijouchou.png",
-    src: "music/Sirius's Heart - Isekaijouchou.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752898/Sirius_s_Heart_-_Isekaijouchou_qrmptl.mp3"
   },
   {
     id: "heart",
     title: "Sirius no Shinzou",
     artist: "Suzuya Aki",
     cover: "img/music/sirius.png",
-    src: "music/Heart of Sirius - 鈴谷アキ.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752891/Heart_of_Sirius_-_%E9%88%B4%E8%B0%B7%E3%82%A2%E3%82%AD_tbeqsu.mp3"
   },
   {
     id: "uminaoshi",
     title: "Uminaoshi",
     artist: "MARETU",
     cover: "img/music/uminaoshi.png",
-    src: "music/Uminaoshi - MARETU.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752905/Uminaoshi_-_MARETU_j9nzuh.mp3"
   },
   {
     id: "tf3",
     title: "The Flight III",
     artist: "Skyper",
     cover: "img/music/theflightiii.png",
-    src: "music/tf3 - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752899/tf3_-_Skyper_xpbuvu.mp3"
   },
   {
     id: "tf6",
     title: "The Flight VI",
     artist: "Skyper",
     cover: "img/music/theflightvi.png",
-    src: "music/tf6 - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752901/tf6_-_Skyper_mbzt4l.mp3"
   },
   {
     id: "tf8",
     title: "The Flight VIII",
     artist: "Skyper",
     cover: "img/music/theflightviii.png",
-    src: "music/tf8 - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752900/tf8_-_Skyper_yl2nsl.mp3"
   },
   {
     id: "tomorrow",
     title: "Tomorrow",
     artist: "Skyper",
     cover: "img/music/tomorrow.png",
-    src: "music/Tomorrow - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752901/Tomorrow_-_Skyper_k2uluc.mp3"
   },
   {
     id: "love",
     title: "Love Again",
     artist: "Skyper",
     cover: "img/music/loveagain.png",
-    src: "music/Love Again - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752892/Love_Again_-_Skyper_ifdipw.mp3"
   },
   {
     id: "with",
     title: "With You",
     artist: "Skyper",
     cover: "img/music/withyou.png",
-    src: "music/With You - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752902/With_You_-_Skyper_mdcpbj.mp3"
   },
   {
     id: "imissyou",
     title: "I Miss You",
     artist: "Skyper",
     cover: "img/music/imissyou.png",
-    src: "music/imissyou - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752893/imissyou_-_Skyper_oqlmpc.mp3"
   },
   {
     id: "hero",
     title: "Hero",
     artist: "Skyper",
     cover: "img/music/heroskyper.png",
-    src: "music/Hero - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752891/Hero_-_Skyper_uzj6q2.mp3"
   },
   {
     id: "tf2077",
     title: "The Flight 2077",
     artist: "Skyper",
     cover: "img/music/theflight2077.png",
-    src: "music/tf2077 - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752900/tf2077_-_Skyper_nwxlmd.mp3"
   },
   {
     id: "why",
     title: "Why Love Me?",
     artist: "Skyper",
     cover: "img/music/whyloveme.png",
-    src: "music/Why Love Me - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752902/Why_Love_Me_-_Skyper_g41bvt.mp3"
   },
   {
     id: "whyii",
     title: "Why Love Me II?",
     artist: "Skyper",
     cover: "img/music/whylovemeii.png",
-    src: "music/WhyII Love Me - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752902/WhyII_Love_Me_-_Skyper_w8dhyn.mp3"
   },
   {
     id: "try",
     title: "Try",
     artist: "Skyper",
     cover: "img/music/try.png",
-    src: "music/Try - Skyper.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752901/Try_-_Skyper_kupso7.mp3"
   },
   {
     id: "onegai",
     title: "Please! KonKon Inari-sama",
     artist: "Nanahira",
     cover: "img/music/konkon.png",
-    src: "music/Onegai Kon-kon Olnari-Sama - Nanahira.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752895/Onegai_Kon-kon_Olnari-Sama_-_Nanahira_hfji6m.mp3"
   },
   {
     id: "soar",
     title: "Soar In The Wind",
     artist: "HoyoMIX",
     cover: "img/music/ayaka.png",
-    src: "music/Soar in the Wind - HoyoMIX.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752898/Soar_in_the_Wind_-_HoyoMIX_o9mj3e.mp3"
   },
   {
     id: "pink",
     title: "Pink",
     artist: "MARETU",
     cover: "img/music/pink.png",
-    src: "music/Pink - MARETU.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752897/Pink_-_MARETU_ku4cib.mp3"
   },
   {
     id: "tengoku",
     title: "Let's Go To Heaven",
     artist: "Kikuo",
     cover: "img/music/tengoku.png",
-    src: "music/Tengoku he ikou - Kikuo.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752899/Tengoku_he_ikou_-_Kikuo_ligynd.mp3"
   },
   {
     id: "down",
     title: "Down In The Shade",
     artist: "Oatmello",
     cover: "img/music/kazuha.png",
-    src: "music/Down In The Shade - Oatmello.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752888/Down_In_The_Shade_-_Oatmello_jkgkji.mp3"
   },
   {
     id: "remembrance",
     title: "Remembrance",
     artist: "Kerusu",
     cover: "img/music/remembrance.png",
-    src: "music/Remembrance - Kerusu.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752896/Remembrance_-_Kerusu_gf17xa.mp3"
   },
   {
     id: "first",
     title: "First Snow",
     artist: "Kerusu",
     cover: "img/music/firstsnow.png",
-    src: "music/First Snow - Kerusu.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752887/First_Snow_-_Kerusu_sewscs.mp3"
   },
   {
     id: "lanterns",
     title: "Lanterns",
     artist: "Xomu",
     cover: "img/music/lanterns.png",
-    src: "music/Lanterns - Xomu.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752895/Lanterns_-_Xomu_idwsg2.mp3"
   },
   {
     id: "iwot",
     title: "I Was Only Temporary",
     artist: "My Head Is Empty",
     cover: "img/music/onlytemporary.png",
-    src: "music/Iwot - My head is empty.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752887/Iwot_-_My_head_is_empty_ydzfah.mp3"
   },
   {
     id: "snowfall",
     title: "Snowfall",
     artist: "Oneheart x Reidenshi",
     cover: "img/music/snowfall.png",
-    src: "music/Snowfall - Reidenshi x Oneheart.mp3"
+    src: "https://res.cloudinary.com/dh5ovya91/video/upload/v1749752899/Snowfall_-_Reidenshi_x_Oneheart_xcpmss.mp3"
   },
 
 
