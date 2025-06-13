d1b5c6b66a523fad8a1e139b2c221bc65980d17d
dlmkjadragon <gogobao21022006@gmail.com>
2025-06-14 05:02:42 +0700

    Create arts-data.js

---
+++ js/arts-data.js
@@ -0,0 +1,30 @@
+const artsData = [
+  {
+    title: "Reisune in Spirit Shrine",
+    image: "https://f2.toyhou.se/file/f2-toyhou-se/images/100495178_uEZM0OQhMemi8lv.png?1747570890",
+    fullImage: "https://f2.toyhou.se/file/f2-toyhou-se/images/100495178_uEZM0OQhMemi8lv.png?1747570890",
+    alt: "Reisune",
+    category: "Halfbody",
+    character: "Reisune",
+    date: "2025-05-18",
+    owner: {
+      name: "Neisy",
+      link: "https://toyhou.se/Neisy"
+    }
+  },
+
+  {
+    title: "Naomi's Reference",
+    image: "https://f2.toyhou.se/file/f2-toyhou-se/thumbnails/101147764_csk.png",
+    fullImage: "https://f2.toyhou.se/file/f2-toyhou-se/images/101147764_csksVFgXr1eRjJG.png", 
+    alt: "Naomi",
+    category: "Reference Sheet",
+    character: "Naomi",
+    date: "2025-05-30",
+    owner: {
+      name: "Neisy",
+      link: "https://toyhou.se/Neisy"
+    }
+  },
+
+];
