0dd5472c1429bb5f6b6496c02bd7e07da579f479
dlmkjadragon <gogobao21022006@gmail.com>
2025-06-24 10:04:01 +0700

    Add files via upload

---
+++ js/characters-data.js
@@ -0,0 +1,55 @@
+const characterCategories = [
+  {
+    category: "Fallen",
+    factions: [
+      {
+        name: "Demon Slayer",
+        banner: "img/banner-demonslayer.png",
+        description: "An elite force forged to eliminate corrupted souls and demons.",
+        characters: [
+          {
+            name: "Kai",
+            title: "- The Fallen Dragon -",
+            image: "https://f2.toyhou.se/file/f2-toyhou-se/thumbnails/99330046_eV1.png",
+            link: "#",
+            icon: "fa-solid fa-dragon"
+          }
+        ]
+      },
+      {
+        name: "Zodiac Pursuit",
+        banner: "img/banner-zodiac.png",
+        description: "Star-guided agents who execute missions in silence.",
+        characters: [
+          {
+            name: "Noir",
+            title: "- Black Flame -",
+            image: "img/noir.png",
+            link: "#",
+            icon: "fa-solid fa-fire"
+          }
+        ]
+      }
+    ]
+  },
+
+  {
+    category: "Spring",
+    factions: [
+      {
+        name: "Flower Garden",
+        banner: "img/banner-flowers.png",
+        description: "Nature's chosen defenders, bringing life to every corner.",
+        characters: [
+          {
+            name: "Iris",
+            title: "- Blooming Light -",
+            image: "img/iris.png",
+            link: "#",
+            icon: "fa-solid fa-leaf"
+          }
+        ]
+      }
+    ]
+  }
+];
