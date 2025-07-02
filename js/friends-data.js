898bd4d8cfdfa91d32312b83658dc2fb785b50f8
dlmkjadragon <gogobao21022006@gmail.com>
2025-07-03 03:26:33 +0700

    Update friends-data.js

--- js/friends-data.js
+++ js/friends-data.js
@@ -1,27 +1,27 @@
 const friendsData = [
   {
     name: "Snowpaws",
-    avatar: "img/friends/snowpaws.png",
+    avatar: "https://res.cloudinary.com/dh5ovya91/image/upload/v1750992926/snowpaws_ilmvqb.png",
     link: "https://toyhou.se/SnowPaws"
   },
   {
     name: "Trick",
-    avatar: "img/friends/trick.jpg",
+    avatar: "https://res.cloudinary.com/dh5ovya91/image/upload/v1750992926/trick_dqyppc.jpg",
     link: "https://www.facebook.com/margaret.ngwyzn"
   },
   {
     name: "Sha",
-    avatar: "img/friends/sha.png",
+    avatar: "https://res.cloudinary.com/dh5ovya91/image/upload/v1750992925/sha_pd96er.jpg",
     link: "https://www.facebook.com/HanSha206"
   },
   {
     name: "Cheryl",
-    avatar: "img/friends/cheryl.webp",
+    avatar: "https://res.cloudinary.com/dh5ovya91/image/upload/v1750992954/cheryl_lqym8m.jpg",
     link: "https://www.instagram.com/cheryl_dog6168/"
   },
   {
     name: "FohyX",
-    avatar: "img/friends/fohyx.jpg",
+    avatar: "https://res.cloudinary.com/dh5ovya91/image/upload/v1750992926/fohyx_kj0umz.jpg",
     link: "https://www.facebook.com/draco.weaver.2024"
   },
 
